031468e6677af28a3acc09a27b9136f334af0efc
Renz Carlo <needkoto17@gmail.com>
2014-11-27 17:47:44 +0800

    second commit

--- slider.js
+++ slider.js
@@ -1,16 +1,16 @@
 'use strict';
 
 $(function() {
-
     var width = 720;
-    var animationSpeed = 1000;
+    var animationSpeed = 500;
     var pause = 3000;
     var currentSlide = 1;
 
     var $slider = $('#slider');
     var $slideContainer = $('.slides', $slider);
     var $slides = $('.slide', $slider);
-
+    var $next = $('#rightNav');
+    var $prev = $('#leftNav');
     var interval;
 
     function startSlider() {
@@ -23,14 +23,32 @@ $(function() {
             });
         }, pause);
     }
+
     function pauseSlider() {
         clearInterval(interval);
     }
 
+function nextSlide() {
+            ++currentSlide;
+            $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
+                if (currentSlide === $slides.length) {
+                    currentSlide = 1;
+                    $slideContainer.css('margin-left', 0);
+                } 
+            });
+    }
+  function prevSlide() {
+            currentSlide = currentSlide -1;
+            if(currentSlide < 1){ 
+                currentSlide = 3;  
+             }  $slideContainer.animate({'margin-left': '+='+width}, animationSpeed);}
+
+
     $slideContainer
-        .on('mouseenter', pauseSlider)
-        .on('mouseleave', startSlider);
+        .on('click', pauseSlider);
 
-    startSlider();
-
+    $next.on('click', nextSlide);
+    $prev.on('click', prevSlide);
+    window.onload(startSlider())
 });
+    
