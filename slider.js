c5194b922f6ecd07cf82c26fde1ff03eac181a04
Renz Carlo <needkoto17@gmail.com>
2014-11-29 21:48:26 +0800

    test.css 	test.html 	test.js

--- slider.js
+++ slider.js
@@ -1,55 +1,57 @@
 'use strict';
 
 $(function() {
-    var width = 720;
+    var width = 693;
     var animationSpeed = 500;
     var pause = 3000;
-    var currentSlide = 1;
-
     var $slider = $('#slider');
     var $slideContainer = $('.slides', $slider);
-    var $slides = $('.slide', $slider);
     var $next = $('#rightNav');
     var $prev = $('#leftNav');
     var interval;
+    var slideCount = $('#slider ul li').length;
+    var slideWidth = $('#slider ul li').width();
+    var slideHeight = $('#slider ul li').height();
+    var sliderUlWidth = slideCount * slideWidth;
+    
+    $('#slider').css({ width: slideWidth, height: slideHeight });
+    
+    $('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
+    
+    $('#slider ul li:last-child').prependTo('#slider ul');
+
 
     function startSlider() {
         interval = setInterval(function() {
-            $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
-                if (++currentSlide === $slides.length) {
-                    currentSlide = 1;
-                    $slideContainer.css('margin-left', 0);
-                } 
-            });
-        }, pause);
+        nextSlide();}, pause);
     }
 
     function pauseSlider() {
         clearInterval(interval);
     }
 
-function nextSlide() {
-            ++currentSlide;
-            $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
-                if (currentSlide === $slides.length) {
-                    currentSlide = 1;
-                    $slideContainer.css('margin-left', 0);
-                } 
-            });
+    function nextSlide() {
+        $('#slider ul li:first-child').appendTo('#slider ul');
+        $('#slider ul').css('margin-left', 0);
+        $slideContainer.animate({'margin-left': '-693px'}, animationSpeed);
+
     }
-  function prevSlide() {
-            currentSlide = currentSlide -1;
-            if(currentSlide < 1){ 
-                currentSlide = 3;  
-             }  $slideContainer.animate({'margin-left': '+='+width}, animationSpeed);}
-
-
-    $slideContainer
-        .on('mouseenter', pauseSlider)
-        .on('mouseleave', startSlider);
-
-    $next.on('click', nextSlide).on('mouseenter',pauseSlider);
-    $prev.on('click', prevSlide).on('mouseenter', pauseSlider);
-   startSlider();
-});
     
+    function prevSlide() {
+        
+         $('#slider ul li:last-child').prependTo('#slider ul');
+         $('#slider ul').css('left', '0px');
+         $slideContainer.animate({'margin-left': '-693px'}, animationSpeed);}
+
+
+        $slideContainer
+        .on('click', pauseSlider);
+
+        $next
+        .on('click', nextSlide).on('mouseenter', pauseSlider);
+        
+        $prev
+        .on('click', prevSlide);
+        
+        startSlider();
+    });
